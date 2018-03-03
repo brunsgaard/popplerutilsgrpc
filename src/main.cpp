7d4c6de7fea7dce55323f9529e8ffcf96d411432
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2018-03-03 20:59:39 +0100

    Added poppler proto

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
 
 #include "poppler-document.h"
+#include "poppler-page.h"
+#include <poppler-document.h>
+#include <poppler-image.h>
+#include <poppler-page.h>
+#include <poppler-page-renderer.h>
 
-int main(int argc, char *argv[])
+
+#include "popplerutilsgrpc.pb.h"
+
+using popplerutilsgrpc::PdftoppmRequest;
+
+
+
+static void error(const std::string &msg)
 {
-   std::cout << "Hello CMake!" << std::endl;
-   return 0;
+    std::cerr << "Error: " << msg << std::endl;
+    std::cerr << "Exiting..." << std::endl;
+    exit(1);
+}
+
+int render(std::string file_name)
+{
+    std::auto_ptr<poppler::document> doc(poppler::document::load_from_file(file_name));
+    if (!doc.get()) {
+        error("loading error");
+    }
+    
+    int doc_page = 0;
+    std::auto_ptr<poppler::page> p(doc->create_page(doc_page));
+    if (!p.get()) {
+        error("NULL page");
+    }
+
+    poppler::page_renderer pr;
+    pr.set_render_hint(poppler::page_renderer::antialiasing, true);
+    pr.set_render_hint(poppler::page_renderer::text_antialiasing, true);
+
+    poppler::image img = pr.render_page(p.get(), 300, 300);
+    if (!img.is_valid()) {
+        error("rendering failed");
+    }
+
+    if (!img.save("outout.png", "png")) {
+        error("saving to file failed");
+    }
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    const std::string file_name(argv[1]);
+    render(file_name);
+    return 0;
 }
