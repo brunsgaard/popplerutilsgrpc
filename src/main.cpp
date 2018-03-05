82d73d5a4f1874a868a1763c4bc34d38809ff253
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2018-03-05 09:52:37 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+#include "goo/PNGWriter.h"
 
 #include "poppler-document.h"
 #include "poppler-page.h"
@@ -9,6 +14,7 @@
 
 
 #include "popplerutilsgrpc.pb.h"
+
 
 using popplerutilsgrpc::PdftoppmRequest;
 
@@ -49,9 +55,41 @@ int render(std::string file_name)
     return 0;
 }
 
+int render_proto()
+{
+    std::cout  << "Output sentence";
+    return 1;
+}
+
+
 int main(int argc, char* argv[])
 {
-    const std::string file_name(argv[1]);
-    render(file_name);
+    const std::string filename (argv[1]);
+
+    std::ifstream ifs (filename, std::ios::binary|std::ios::ate);
+    std::ifstream::pos_type pos = ifs.tellg();
+
+    //std::vector<char> result (pos);
+    poppler::byte_array result (pos); 
+
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(&result[0], pos);
+    std::auto_ptr<poppler::document> doc(poppler::document::load_from_data(&result));
+    int doc_page = 0;
+    std::auto_ptr<poppler::page> p(doc->create_page(doc_page));
+    poppler::page_renderer pr;
+    pr.set_render_hint(poppler::page_renderer::antialiasing, true);
+    pr.set_render_hint(poppler::page_renderer::text_antialiasing, true);
+
+    poppler::image img = pr.render_page(p.get(), 300, 300);
+
+    std::string fmt = "png";
+    const int actual_dpi = 75;
+
+    std::unique_ptr<ImgWriter> w;
+    //w.reset(new PNGWriter());
+
+
+
     return 0;
 }
